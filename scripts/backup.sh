02b27ffd485e34d2b0b5afef8bffb94c1ebfae8d
Robert Brennan <bobby.brennan@gmail.com>
2019-11-20 21:10:45 +0000

    finalize backup and restore scripts

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 set -e
 
-echo dirname "$0"
 source `dirname "$0"`/util.sh
 
 set -euo pipefail
+cur_directory=$(pwd)
 
 rm -rf ~/k8s-backup/*
 now=`date +%s`
@@ -21,8 +21,10 @@ kubectl get pv | while read line ; do
   name=${name/\//.}
   id=$(get_string "${line}" 1)
 
-  sudo zip -r ~/k8s-backup/$now/$name.zip ~/kind-disk/$id
+  cd ~/kind-disk/$id
+  sudo zip -r ~/k8s-backup/$now/$name.zip . -i \*
 done
 
+cd $cur_directory
 /usr/local/bin/aws2 s3 sync ~/k8s-backup/ s3://bb-kubernetes-backups --profile k8s-upload-backup
 echo -e '\n\n'"Created backup snapshot $now"'\n\n'
--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,6 +1,33 @@
 #! /bin/bash
 set -e
 
+source `dirname "$0"`/util.sh
+
 echo "Restoring $1 from snapshot at $2"
-aws2 s3 copy s3://bb-kubernetes-backups/$2/$1.zip ./ --profile k8s-upload-backup
-unzip $1.zip
+rm -rf restore
+mkdir restore
+aws2 s3 cp s3://bb-kubernetes-backups/$2/$1.zip ./restore/ --profile k8s-upload-backup
+cd restore && unzip -q $1.zip && cd ..
+rm restore/$1.zip
+
+idx=0
+while read -r line; do
+  idx=$(( $idx + 1))
+  if [ $idx -eq 1 ]; then
+    continue
+  fi
+
+  name=$(get_string "${line}" 6)
+  name=${name/\//.}
+  id=$(get_string "${line}" 1)
+  if [[ $name == $1 ]]; then
+    echo "found $id"
+    sudo mv ~/kind-disk/$id ~/kind-disk/backup-$id || true
+    sudo mv ./restore ~/kind-disk/$id
+    echo "Successfully restored $1 from $2 to PV $id"
+    exit 0
+  fi
+done < <(kubectl get pv)
+
+echo "PV not found!"
+exit 1
