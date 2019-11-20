e8b2b5e4c045d966d5f57279b47dfe61d3860d74
Robert Brennan <bobby.brennan@gmail.com>
2019-11-20 13:54:02 +0000

    fix up backup script

--- backup.sh
+++ backup.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 mkdir -p ~/k8s-backup/
 zip -r ~/k8s-backup/`date +%s`.zip ~/kind-disk/
-aws2 s3 sync ~/k8s-backup/ s3://bb-kubernetes-backups --profile k8s-upload-backup
+/usr/local/bin/aws2 s3 sync ~/k8s-backup/ s3://bb-kubernetes-backups --profile k8s-upload-backup
