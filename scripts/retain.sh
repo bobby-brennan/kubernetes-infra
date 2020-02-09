457d7eb4619d2cff57ec08e4b6737fa387590af4
Robert Brennan <bobby.brennan@gmail.com>
2020-02-09 18:06:29 +0000

    add support for digital ocean backups

--- scripts/retain.sh
+++ scripts/retain.sh
@@ -14,3 +14,14 @@ while read -r line; do
   kubectl patch pv $id -p '{"spec":{"persistentVolumeReclaimPolicy":"Retain"}}'
 done < <(kubectl get pv -A)
 
+idx=0
+while read -r line; do
+  idx=$(( $idx + 1))
+  if [ $idx -eq 1 ]; then
+    continue
+  fi
+  ns=$(get_string "${line}" 1)
+  id=$(get_string "${line}" 2)
+  echo "patching pvc $ns/$id"
+  kubectl patch pvc $id -n $ns -p '{"metadata": {"annotations": {"backup.kubernetes.io/deltas": "P1D P30D"}}}'
+done < <(kubectl get pvc -A)
