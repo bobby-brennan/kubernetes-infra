c48043fdf8a8f9122a33876a56514f16d7498011
Robert Brennan <bobby.brennan@gmail.com>
2020-04-07 16:28:49 +0000

    move some things around for KIND

--- scripts/create-kind.sh
+++ scripts/create-kind.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 set -eo pipefail
 
-kind create cluster --config kind-cluster.yaml
+kind create cluster --config kind/cluster.yaml
 
-kubectl apply -f local-path-storage.yaml
+kubectl apply -f kind/local-path-storage.yaml
 kubectl patch storageclass standard -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false", "storageclass.beta.kubernetes.io/is-default-class":"false"}}}'
 kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true", "storageclass.beta.kubernetes.io/is-default-class":"true"}}}'
 
